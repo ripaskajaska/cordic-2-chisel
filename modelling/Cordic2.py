e7334a337cc2d534fec950a3fa973ac9a2ba7666
Roope Jaskari <roope.jaskari@aalto.fi>
2026-03-29 15:36:45 +0300

    Some changes passed to another pc

--- modelling/Cordic2.py
+++ modelling/Cordic2.py
@@ -12,9 +12,11 @@ class Cordic2:
     stage_7_bis_rotations = [ i * 0.056 for i in range (0, 8) ]
     # Cordic 2 performs single iteration of each of its stage.
     def __init__(self, x: int, y: int, z: int):
+        
         self.x: int = x
         self.y: int = y
         self.z: int = z
+        self.quadrant = 0
 
     def cordic(self, x: int, y: int, a_in: int):
         pass
@@ -43,7 +45,42 @@ class Cordic2:
             else:
                 self.x, self.y, self.z = x, y, a_in
 
-        
+    def quadrant_select(self, a_in: int):
+        if a_in < 0:
+            if -45 <= a_in:
+                self.quadrant = 0
+            elif -135 <= a_in < -45:
+                self.quadrant = 1
+            elif -225 <= a_in < -135:
+                self.quadrant = 2
+            elif -315 <= a_in < -225:
+                self.quadrant = 3
+            else:
+                self.quadrant = 0
+        else:
+            if 45 < a_in <= 135:
+                self.quadrant = 1
+            elif 135 < a_in <= 225:
+                self.quadrant = 2
+            elif 225 < a_in <= 315:
+                self.quadrant = 3
+            else:
+                self.quadrant = 0
+    
+    def normalize_angle(self, a_in: int):
+        if self.quadrant == 0:
+            self.z = a_in * (2**15 / 90)
+    
+    
+    def quadrant_correction(self, x: int, y: int):
+        if self.quadrant == 0:
+            self.x, self.y = x, y
+        elif self.quadrant == 1:
+            self.x, self.y = -y, x
+        elif self.quadrant == 2:
+            self.x, self.y = -x, -y
+        elif self.quadrant == 3:
+            self.x, self.y = y, -x
 
     def friend_angles(self, x: int, y: int, a_in: int):
         # select rotation angle and sign based on input angle
