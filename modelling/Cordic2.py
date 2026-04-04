b8888802f6b9bfa30c030e70690b0b5c1cba3844
ripaskajaska <roope.jaskari@aalto.fi>
2026-04-04 18:04:59 +0300

    add changes

--- modelling/Cordic2.py
+++ modelling/Cordic2.py
@@ -2,14 +2,14 @@ from Cordic import Cordic
 from math import pi
 class Cordic2:
 
-    stage_1_rotations = [0, 90, 180, 270]
-    stage_2_rotations = [0, 16.260, 36.870]
-    stage_3_rotations = [0, 7.125]
-    stage_4_rotation = 1.790
-    stage_5_rotation = 0.895
-    stage_6_rotations = [ i * 0.112 for i in range (0, 8) ]
-    stage_6_bis_rotation = 0.448
-    stage_7_bis_rotations = [ i * 0.056 for i in range (0, 8) ]
+    stage_1_rotations = [0, 2**14, 2**15]
+    stage_2_rotations = [0, int(16.260 * (2**15 / 180)), 36.870 * (2**15 / 180)]
+    stage_3_rotations = [0, int(7.125 * (2**15 / 180))]
+    stage_4_rotation = int(1.790 * (2**15 / 180))
+    stage_5_rotation = int(0.895 * (2**15 / 180))
+    stage_6_rotations = [ i * int(0.112 * (2**15 / 180)) for i in range (0, 8) ]
+    stage_6_bis_rotation = int(0.448 * (2**15 / 180))
+    stage_7_bis_rotations = [ i * int(0.056 * (2**15 / 180)) for i in range (0, 8) ]
     # Cordic 2 performs single iteration of each of its stage.
     def __init__(self, x: int, y: int, z: int):
         
@@ -18,79 +18,113 @@ class Cordic2:
         self.z: int = z
         self.quadrant = 0
 
-    def cordic(self, x: int, y: int, a_in: int):
+    def cordic(self):
         pass
         # select rotation angle and sign based on input angle
         
-    def trivial_rotations(self, x: int, y: int, a_in: int):
+    def trivial_rotations(self):
+
+        # trivial rotations to reduce the input angle to [-45, 45[
+
+        if self.z < 0:
+            # 2**13 corresponds to 45 degrees
+            # 2**14 corresponds to 90 degrees
+            # 2**15 corresponds to 180 degrees
+            if -2**13 * 3 <= self.z < -2**13:
+                self.x, self.y, self.z = self.y, -self.x, self.z + self.stage_1_rotations[1]
+            elif -2**15 <= self.z < -2**13 * 3:
+                self.x, self.y, self.z = -self.x, -self.y, self.z + self.stage_1_rotations[2]
+            else:
+                # No need for changes
+                self.x, self.y, self.z = self.x, self.y, self.z
+        else:
+            if 2**13 < self.z <= 2**13 * 3:
+                self.x, self.y, self.z = -self.y, self.x, self.z - self.stage_1_rotations[1]
+            elif 2**13 * 3 < self.z <= 2**15:
+                self.x, self.y, self.z = -self.x, -self.y, self.z - self.stage_1_rotations[2]
+            else:
+                self.x, self.y, self.z = self.x, self.y, self.z
+
+    def normalize_angle(self):
+        self.z = self.z % 360
+        # normalize angle to [-180, 180[
+        if self.z > 180:
+            self.z -= 360
+        if self.z < -180:
+            self.z += 360
+        if self.z == 180:
+            self.z = -180
+        # convert to fixed-point representation
+        self.z = int(self.z * (2**15 / 180))
+
+    def friend_angles_bit_accurate(self):
+        # kernel rotation select
+        goal_max_magnitude = int(10.305 * (2**15 / 180))
+        angle_negative = self.z < 0
+        if angle_negative:
+            if self.z < -self.stage_2_rotations[2]:
+                rotation_idx = 2
+            elif self.z < -(self.stage_2_rotations[1] + goal_max_magnitude):
+                rotation_idx = 2
+            elif self.z < -self.stage_2_rotations[1]:
+                rotation_idx = 1
+            else:
+                rotation_idx = 0
+        else:
+            if self.z > self.stage_2_rotations[2]:
+                rotation_idx = 2
+            elif self.z > self.stage_2_rotations[1] + goal_max_magnitude:
+                rotation_idx = 2
+            elif self.z > self.stage_2_rotations[1]:
+                rotation_idx = 1
+            else:
+                rotation_idx = 0
+        X_sum = self.x << 1
+        if rotation_idx != 0:
+            if not angle_negative:
+                X_sum -= self.y << 1
+            else:
+                X_sum += self.y << 1
+        else:
+            if not angle_negative:
+                X_sum -= self.x
+            else:
+                X_sum += self.x
+        if rotation_idx == 1:
+            X_sum == X_sum << 2
+        else:
+            X_sum == X_sum << 3
+
+        
+        
+        Y_sum = self.y << 4
+        if rotation_idx != 0:
+            if not angle_negative:
+                Y_sum -= self.x
+            else:
+                Y_sum += self.x
+        else: # rotation_idx == 0
+            if not angle_negative:
+                Y_sum -= self.y << 3
+            else:
+                Y_sum += self.y << 3
+        
+
+        if rotation_idx == 1:
+            X_sum += self.x << 5 + self.y
+        
+            
+
+
+
         # select rotation angle and sign based on input angle
-        if a_in < 0:
-            if -45 <= a_in:
-                self.x, self.y, self.z = x, y, a_in
-            elif -135 <= a_in < -45:
-                self.x, self.y, self.z = y, -x, a_in + 90
-            elif -225 <= a_in < -135:
-                self.x, self.y, self.z = -x, -y, a_in + 180
-            elif -315 <= a_in < -225:
-                self.x, self.y, self.z = -y, x, a_in + 270
-            else:
-                self.x, self.y, self.z = x, y, a_in
-        else:
-            if 45 < a_in <= 135:
-                self.x, self.y, self.z = -y, x, a_in - 90
-            elif 135 < a_in <= 225:
-                self.x, self.y, self.z = -x, -y, a_in - 180
-            elif 225 < a_in <= 315:
-                self.x, self.y, self.z = y, -x, a_in - 270
-            else:
-                self.x, self.y, self.z = x, y, a_in
+        # implementation of the friend angle stage with modelling real hardware behaviour
 
-    def quadrant_select(self, a_in: int):
-        if a_in < 0:
-            if -45 <= a_in:
-                self.quadrant = 0
-            elif -135 <= a_in < -45:
-                self.quadrant = 1
-            elif -225 <= a_in < -135:
-                self.quadrant = 2
-            elif -315 <= a_in < -225:
-                self.quadrant = 3
-            else:
-                self.quadrant = 0
-        else:
-            if 45 < a_in <= 135:
-                self.quadrant = 1
-            elif 135 < a_in <= 225:
-                self.quadrant = 2
-            elif 225 < a_in <= 315:
-                self.quadrant = 3
-            else:
-                self.quadrant = 0
-    
-    def normalize_angle(self, a_in: int):
-        if self.quadrant == 0:
-            self.z = a_in * (2**15 / 90)
-    
-    
-    def quadrant_correction(self, x: int, y: int):
-        if self.quadrant == 0:
-            self.x, self.y = x, y
-        elif self.quadrant == 1:
-            self.x, self.y = -y, x
-        elif self.quadrant == 2:
-            self.x, self.y = -x, -y
-        elif self.quadrant == 3:
-            self.x, self.y = y, -x
-
-    def friend_angles(self, x: int, y: int, a_in: int):
+    def usr_cordic(self):
         # select rotation angle and sign based on input angle
         pass
 
-    def usr_cordic(self, x: int, y: int, z: int, n: int):
-        # select rotation angle and sign based on input angle
-        pass
-
-    def nano_rotations(self, x: int, y: int, z: int, n: int):
+    def nano_rotations(self):
         # select rotation angle and sign based on input angle
         pass
     
